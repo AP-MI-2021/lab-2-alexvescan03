4ce3d854098a848db5bcfe33bf2ab410bf07c105
alexvescan03 <91516501+alexvescan03@users.noreply.github.com>
2021-10-04 14:43:39 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
-def is_palindrome(n):
-    cn=n
-    inv=0
-    while cn>0:
-        inv=inv*10 + cn%10
-        cn=cn//10
-    ok=False
-    if inv==n:
-        ok=True
-    if ok==True:
-        print("da")
-    else:
-        print("nu")
+def get_largest_prime_below(x):
+    if(x<=1):
+        return False
+    if(x==2):
+        return True
+    if(x%2==0):
+        return False
 
-def main():
-    n=int(input("n="))
-    m=is_palindrome(n)
-    print(m)
-main()
+    i=3
+    while(i*i<=x):
+        if(x%i==0):
+            return False
+        i+=2
+    return True
 
+n=int(input("n="))
 
+for i in range(n-1,1,-1):
+    if(get_largest_prime_below(i)):
+        print("numarul {} este prim".format(i))
+        break
