254aeeedf5949d7b7eab78effa26424915b4aa9b
alexvescan03 <91516501+alexvescan03@users.noreply.github.com>
2021-10-05 08:19:31 +0300

    lab 2 problemele 1 2 si 5

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#problema 1
 def get_largest_prime_below(x):
     if(x<=1):
         return False
@@ -19,3 +20,30 @@ for i in range(n-1,1,-1):
     if(get_largest_prime_below(i)):
         print("numarul {} este prim".format(i))
         break
+
+#problema 5
+def is_palindrome(n):
+    cn=n
+    inv=0
+    while cn>0:
+        inv=inv*10 + cn%10
+        cn=cn//10
+    ok=False
+    if inv==n:
+        ok=True
+    if ok==True:
+        print("da")
+    else:
+        print("nu")
+
+def main():
+    n=int(input("n="))
+    m=is_palindrome(n)
+    print(m)
+main()
+
+#problema 2
+def get_age_in_days(day):
+  from datetime import datetime
+  number_of_days_since_day = (datetime.utcnow() - day).days
+  return number_of_days_since_day
